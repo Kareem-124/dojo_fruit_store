19ef2fbd948dd60e7100eda91c39e153610bda3e
Kareem Malhis <kareem-124@hotmail.com>
2023-05-19 22:03:44 +0300

    upload fruits

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect
+import datetime
+
+
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -8,7 +11,35 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    
+    apple_count = int(request.form['apple'])
+    blackberry_count = int(request.form['blackberry'])
+    raspberry_count = int(request.form['raspberry'])
+    strawberry_count = int(request.form['strawberry'])
+    total = int(apple_count) + int(blackberry_count) + int(raspberry_count) + int(strawberry_count)
+    print(f"THe value of apple = {apple_count}\
+        \nThe value of Blackberry = {blackberry_count}\
+        \nThe value of raspberry = {raspberry_count}\
+        \nThe value of strawberry = {strawberry_count}\
+        \n The Total value = {total}")
+    firstname=request.form['first_name']
+    lastname=request.form['last_name']
+    studentid=request.form['student_id']
+    if studentid == "":
+        studentid="N/A"
+    date = datetime.datetime.now()
+
+
+    return render_template("checkout.html" 
+                            ,apple_checkout = int(apple_count)
+                            ,blackberry_checkout = int(blackberry_count)
+                            ,raspberry_checkout = int(raspberry_count)
+                            ,strawberry_checkout = int(strawberry_count) 
+                            ,firstname_checkout = firstname
+                            ,lastname_checkout = lastname
+                            ,studentid_checkout = studentid
+                            ,total_checkout = total
+                            ,date_checkout = date.strftime('%c') )
 
 @app.route('/fruits')         
 def fruits():
